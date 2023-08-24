835a6e6ab79d57c8764e4f4a55482654dcf40437
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2023-08-24 10:46:20 +0530

    Added Dialog for NewBook

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 let myLibrary=[];
+const addBtn=document.querySelector('.addBtn');
+addBtn.addEventListener('click',toAddBook);
+const cardContainer=document.querySelector('.cardContainer');
+
+const newBookDialog=document.querySelector('.newBookDialog');
+
+
 function Book(title,author,pages,read){
     this.title=title;
     this.author=author;
@@ -9,16 +16,17 @@ function Book(title,author,pages,read){
     }
 };
 function toAddBook(){
-    const book1=new Book("Wings1","Ayush",200,"read");
-    myLibrary.push(book1);
-    const book2=new Book("Wings2","Ayush",200,"read");
-    myLibrary.push(book2);
-    const book3=new Book("Wings3","Ayush",200,"read")
-    myLibrary.push(book3);
+    newBookDialog.showModal();
+    // const book1=new Book("Wings1","Ayush",200,"read");
+    // myLibrary.push(book1);
+    // // const book2=new Book("Wings2","Ayush",200,"read");
+    // // myLibrary.push(book2);
+    // // const book3=new Book("Wings3","Ayush",200,"read")
+    // // myLibrary.push(book3);
+    toDisplayBook();
 }
 function toDisplayBook(){
-    // console.log(book1.info());
+    myLibrary.forEach(element => {
+       cardContainer.innerHTML+="<div class='card'>After</div>"; 
+    });
 }
-Book();
-toAddBook();
-toDisplayBook();
