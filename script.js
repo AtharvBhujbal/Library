859614fe1f1cbd62ff271cfd12481e909c97e8cc
Atharv Bhujbal <126101769+AtharvBhujbal@users.noreply.github.com>
2023-08-24 12:39:52 +0530

    Updated Dialog Functionalities and Updated dispaly function

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let myLibrary=[];
 const addBtn=document.querySelector('.addBtn');
 addBtn.addEventListener('click',toAddBook);
 const cardContainer=document.querySelector('.cardContainer');
-
 const newBookDialog=document.querySelector('.newBookDialog');
+const submitBtn=document.querySelector('.submitBtn');
 
 
 function Book(title,author,pages,read){
@@ -17,16 +17,29 @@ function Book(title,author,pages,read){
 };
 function toAddBook(){
     newBookDialog.showModal();
-    // const book1=new Book("Wings1","Ayush",200,"read");
-    // myLibrary.push(book1);
-    // // const book2=new Book("Wings2","Ayush",200,"read");
-    // // myLibrary.push(book2);
-    // // const book3=new Book("Wings3","Ayush",200,"read")
-    // // myLibrary.push(book3);
-    toDisplayBook();
 }
 function toDisplayBook(){
-    myLibrary.forEach(element => {
-       cardContainer.innerHTML+="<div class='card'>After</div>"; 
+    cardContainer.innerHTML = ""; 
+    myLibrary.forEach(book => {
+        const card=document.createElement('div');
+        card.classList.add('card');
+        card.innerHTML=`<h3>${book.title}</h3> <p>Author: ${book.author}</p> <p>Pages: ${book.pages}</p> <p>Status: ${book.read ? 'Read' : 'Not read'}</p>`;
+        cardContainer.appendChild(card);
     });
 }
+submitBtn.addEventListener('click',(event)=>{
+    event.preventDefault();
+    const title=newBookDialog.querySelector('input[name="title"]').value;
+    const author=newBookDialog.querySelector('input[name="author"]').value;
+    const pages=newBookDialog.querySelector('input[name="pages"]').value;
+    const read=document.querySelector('input[name="read"]').checked;
+    const newBook=new Book(title,author,pages,read);
+    myLibrary.push(newBook);
+    newBookDialog.close();
+    toDisplayBook();
+})
+newBookDialog.addEventListener('click', (event) => {
+    if (event.target === newBookDialog) {
+        newBookDialog.close();
+    }
+});
